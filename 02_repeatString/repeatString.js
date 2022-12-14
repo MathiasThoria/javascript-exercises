c7437a0d59d7c4c1acc2e6d9e6109b3964eaf2c2
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-14 18:38:23 -0300

    Ex2 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,times) {
+let concString ="";
 
-};
+if (times >= 0){
+    for (let a = 0; a < times; a++){
+    concString= concString + string;
+    }
+    return concString;
+}else{
+    return "ERROR";
+}
 
+}
 // Do not edit below this line
 module.exports = repeatString;
