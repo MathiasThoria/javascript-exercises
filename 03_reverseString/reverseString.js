9be76f7a29804d305b00a3ae8616353c18ed9ff8
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-14 19:44:52 -0300

    Ex3 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
 
-};
 
+const reverseString = function(string) {
+    let stringTmp = "";
+    console.log(string.slice((string.length)-1));
+    for (let a=0; a < string.length; a++){
+        stringTmp = string.slice(a,a+1) + stringTmp;        
+    }
+    return stringTmp;
+}
+//console.log(reverseString("Hola"));
 // Do not edit below this line
 module.exports = reverseString;
