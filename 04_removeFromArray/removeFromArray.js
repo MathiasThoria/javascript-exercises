00cf794063b4c3a7123e5b203e2688edadd78551
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-15 05:17:16 -0300

    Ex07 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(arr, strFilter) {
     let result;
     for (let a=0; a< arguments.length; a++){
         //console.log(arguments[a+1]);
-        //arr = arr.filter(elem => !((+(elem) == +(arguments[a+1])) || (elem == arguments[a+1]) ));
+        
         arr = arr.filter(elem => !(elem === arguments[a+1]));
     }    
     return arr;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let tmp=0;
+  tmp = ((far-32)*(5/9));
+  if (Number.isInteger(tmp)) {
+    return +tmp;
+  }  else{
+    return +tmp.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let tmp=0;
+  tmp = ((celcius*(9/5))+32);
+  if (Number.isInteger(tmp)){
+    return +tmp;
+  }  else{
+    return +tmp.toFixed(1);
+  }
 };
-
+console.log(convertToCelsius(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
