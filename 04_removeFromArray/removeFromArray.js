8e69b8d29b05e18474aeec18e1656365fe3b6173
MathiasThoria <mathiassoriapiro@gmail.com>
2022-12-15 03:20:47 -0300

    Ex04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, strFilter) {
+    let result;
+    for (let a=0; a< arguments.length; a++){
+        //console.log(arguments[a+1]);
+        //arr = arr.filter(elem => !((+(elem) == +(arguments[a+1])) || (elem == arguments[a+1]) ));
+        arr = arr.filter(elem => !(elem === arguments[a+1]));
+    }    
+    return arr;
+}
+//console.log(removeFromArray([5,2,3],"5",3));
 
-};
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
